805b8dc156482b41c9eaf5e9f5647ef20e9186ba
Aditya Parihar <adityapariharparihar@gmail.com>
2022-11-10 10:48:34 +0530

    ⚠️ testcase could be nums of length 4 🔼 was valid for 3

---
+++ LargestPerimeterTriangle.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+/*Har Har Mahadev*/
+public class LargestPerimeterTriangle 
+{
+public static void main(String[] args) 
+{
+ int[] array = {2,1,2};
+ System.out.println(checkTriangle(array));
+    
+}
+static int checkTriangle(int[] nums) 
+{
+    Arrays.sort(nums);
+    for (int i = nums.length-3; i >= 0; --i)
+    {
+        if (nums[i]+nums[i+1] > nums[i+2]) 
+        {
+          return nums[i]+nums[i+1]+nums[i+2];
+        }
+    }
+ return 0;    
+}    
+}
